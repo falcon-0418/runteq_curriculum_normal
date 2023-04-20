a31e5391389335d963af8d10f336a6171ba9fbec
PIKO <shinsedaikakumei@gmail.com>
2023-04-21 00:24:10 +0900

    修正時のコミットメッセージ

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,21 +4,19 @@ class UserSessionsController < ApplicationController
   end
 
 
-  def create 
+  def create
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to root_path, success: t('.success')
     else
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to login_path
+    redirect_to root_path, success: t('.success')
   end
 end
-
-
-
