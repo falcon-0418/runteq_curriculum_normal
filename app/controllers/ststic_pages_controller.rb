af104d874a0fa7d3717d44b0792137c903bfe36b
PIKO <shinsedaikakumei@gmail.com>
2023-04-28 01:14:17 +0900

    修正時のコミットメッセージ

--- app/controllers/ststic_pages_controller.rb
+++ app/controllers/ststic_pages_controller.rb
@@ -1,5 +1,5 @@
 class StsticPagesController < ApplicationController
-  def top
-  end
-end
+  skip_before_action :require_login, only: %i[top]
 
+  def top; end
+end
--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,9 @@ module RunteqNormal
       # add any other custom generator settings here
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     end
   end
 end
