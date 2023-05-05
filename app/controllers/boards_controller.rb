39870cc858751d6d7bbad9cdde897b9de244ad36
PIKO <shinsedaikakumei@gmail.com>
2023-05-06 02:15:30 +0900

    修正時のコミットメッセージ

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :find_board, only: [:edit, :update, :destroy]
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
   end
@@ -23,7 +24,31 @@ class BoardsController < ApplicationController
     @comments = @board.comments.includes(:user).order(created_at: :desc)
   end
 
+  def edit
+    @board = current_user.boards.find(params[:id])
+  end
+
+  def update
+    @board = current_user.boards.find(params[:id])
+    if @board.update(board_params)
+      redirect_to @board, success: t('defaults.message.updated', item: Board.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.mesage.not_updated', item: Board.model_name.human)
+      render :edit
+    end
+  end
+
+  def destroy
+    @board = current_user.boards.find(params[:id])
+    @board.destroy!
+    redirect_to boards_path, success: t('defaults.message.deleted', item: Board.model_name.human)
+  end
+
   private
+
+  def find_board
+    @board = current_user.boards.find(params[:id])
+  end
 
   def board_params
     params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
